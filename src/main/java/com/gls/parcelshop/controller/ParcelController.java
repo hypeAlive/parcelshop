5ea21f990725d82fc4d935980075a828d9cc4bea
Alive <nicolasfr002@gmail.com>
2024-06-03 18:50:54 +0200

    feat(controller): added route OutForDelivery Parcels
    - route "/parcels/out-for-delivery"
    -> gets all parcels with OUT_FOR_DELIVERY state
    
    - possible queryParam: date ("/parcels/out-for-delivery?date=20200421"
    -> gets all parcels with OUT_FOR_DELIVERY state and Date 20200421

--- src/main/java/com/gls/parcelshop/controller/ParcelController.java
+++ src/main/java/com/gls/parcelshop/controller/ParcelController.java
@@ -1,17 +1,18 @@
 package com.gls.parcelshop.controller;
 
+import com.gls.parcelshop.model.DeliveryState;
 import com.gls.parcelshop.model.Parcel;
 import com.gls.parcelshop.repository.ParcelRepository;
 import com.gls.parcelshop.service.NotificationService;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -33,6 +34,21 @@ public class ParcelController {
         Parcel savedParcel = parcelRepository.save(parcel);
         notificationService.notifySomeoneAboutChange(savedParcel);
         return new ResponseEntity<>(savedParcel, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/parcels/out-for-delivery")
+    public List<Parcel> getParcelsOutForDelivery(
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyyMMdd") Date date) {
+
+        if (date != null) {
+            // If only one date is provided, find parcels for this date
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
+            String dateString = formatter.format(date);
+            return parcelRepository.findByDeliveryDateAndDeliveryState(dateString, DeliveryState.OUT_FOR_DELIVERY);
+        } else {
+            // If no dates are provided, find all parcels out for delivery
+            return parcelRepository.findByDeliveryState(DeliveryState.OUT_FOR_DELIVERY);
+        }
     }
 
 }
